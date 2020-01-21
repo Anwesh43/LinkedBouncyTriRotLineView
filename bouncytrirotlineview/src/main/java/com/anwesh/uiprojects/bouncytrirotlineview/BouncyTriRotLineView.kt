8d29a14c00bc6460f0fb03a83a8f5f167686e6f1
Anwesh43 <anweshthecool0@gmail.com>
2020-01-22 01:29:39 +0530

    adding Canvas extension methods

--- bouncytrirotlineview/src/main/java/com/anwesh/uiprojects/bouncytrirotlineview/BouncyTriRotLineView.kt
+++ bouncytrirotlineview/src/main/java/com/anwesh/uiprojects/bouncytrirotlineview/BouncyTriRotLineView.kt
@@ -26,3 +26,33 @@ fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
 
+fun Canvas.drawTriRotLine(i : Int, scale : Float, size : Float, paint : Paint) {
+    val sf : Float = scale.sinify().divideScale(i, lines)
+    val sj : Float = 1f - 2 * i
+    save()
+    translate(-size, 0f)
+    rotate(-deg * sf * sj)
+    drawLine(0f, 0f, 2 * size * sj, 0f, paint)
+    restore()
+}
+
+fun Canvas.drawTriRotLines(scale : Float, size : Float, paint : Paint) {
+    for (j in 0..(lines - 1)) {
+        drawTriRotLine(j, scale, size, paint)
+    }
+}
+
+fun Canvas.drawBTRLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(gap * (i + 1), h / 2)
+    drawTriRotLines(scale, size, paint)
+    restore()
+}
+
